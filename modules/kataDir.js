f75eacee376057a916f1c069064e11bcdf50d3c4
Dreas Weiss <dreasweiss@gmail.com>
2024-07-15 00:02:54 +0200

    feat: add kata directory creation and file setup functionality
    - Implemented kataDir function to create a new directory for a kata if it doesn't already exist.
    - Added creation of README.md file with kata details.
    - Added creation of kata.js file with the kata solution.

--- modules/kataDir.js
+++ modules/kataDir.js
@@ -1,21 +1,12 @@
 const fs = require("fs");
 const path = require("path");
-
+const createFile = require("./createFile");
 
 async function kataDir(kataInfo) {
   let { title, kyu, text, kataJS, url, titleCamelCase } = kataInfo;
 
-  const readme = `# ${title}
-    ${kyu} kyu
-    [link to kata](${url})
-    <br/>
-    [my solution]('./kata.js')
-    <br/>
-    <br/>
-    ${text}`;
-
+  // create new kata dir
   const kataDirPath = `./katas/${titleCamelCase}`;
-
   if (!fs.existsSync(kataDirPath)) {
     fs.mkdirSync(kataDirPath);
     console.log(`Directory: ${titleCamelCase} - created successfully`);
@@ -24,24 +15,20 @@ async function kataDir(kataInfo) {
   }
 
   // create md file - kata's task info
+  const readme = `# ${title}
+    ${kyu} kyu
+    [link to kata](${url})
+    <br/>
+    [my solution]('./kata.js')
+    <br/>
+    <br/>
+    ${text}`;
   const kataReadmePath = `${kataDirPath}/README.md`;
-  fs.writeFile(kataReadmePath, readme, (err) => {
-    if (err) {
-      console.error("An error occurred while writing the file:", err);
-    } else {
-      console.log("Kata's info file created successfully!");
-    }
-  });
+  createFile(kataReadmePath, readme);
 
   // create kata.js with katas func
   const kataJsPath = `${kataDirPath}/kata.js`;
-  fs.writeFile(kataJsPath, kataJS, (err) => {
-    if (err) {
-      console.error("An error occurred while writing the file:", err);
-    } else {
-      console.log("kata.js file created successfully!");
-    }
-  });
+  createFile(kataJsPath, kataJS);
 }
 
 module.exports = kataDir;
